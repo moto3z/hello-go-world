d79db66fda6be32b9d0cc2e79809c609401c2421
dong <dong.22@kakaoenterprise.com>
2023-09-04 10:42:02 +0900

    gg

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,8 +1,13 @@
 package config
 
-import "html/template"
+import (
+	"html/template"
+	"log"
+)
 
-// AppConfig holds th eapp config
+// AppConfig holds the app config 전체 앱에서 공유하는거라서 퍼블릭으로 열어줘야함
 type AppConfig struct {
+	UseCache      bool
 	TemplateCache map[string]*template.Template
+	InfoLog       *log.Logger
 }
