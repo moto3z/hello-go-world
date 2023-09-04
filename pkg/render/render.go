a7e86ec98f47f4aa8eba944bf2dc416b27816fe1
dong <dong.22@kakaoenterprise.com>
2023-09-04 10:22:48 +0900

    gg

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -1,21 +1,76 @@
 package render
 
 import (
+	"bytes"
 	"html/template"
+	"log"
 	"net/http"
+	"path/filepath"
 )
 
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
+	//create a template cache
+	tc, err := createTemplateCache()
+	if err != nil {
+		log.Fatal(err)
+	}
+	//get request from cache
+	t, ok := tc[tmpl]
+	if !ok {
+		log.Fatal(err)
+	}
+	buf := new(bytes.Buffer)
+	err = t.Execute(buf, nil)
+	if err != nil {
+		log.Println(err)
+	}
+
+	//render the tmpl
+	_, err = buf.WriteTo(w)
+	if err != nil {
+		log.Println(err)
+	}
+
+	//before code
 	parsedTemplate, _ := template.ParseFiles("./templates/"+tmpl, "./templates/base.layout.tmpl")
-	err := parsedTemplate.Execute(w, nil)
+	err = parsedTemplate.Execute(w, nil)
 	if err != nil {
 		print("error")
 	}
 }
 
-var tc = make(map[string]*template.Template)
+func createTemplateCache() (map[string]*template.Template, error) {
+	//myCache := make(map[string]*template.Template)
+	myCache := map[string]*template.Template{}
+	// get all of the files named "*.page.tmpl"
+	pages, err := filepath.Glob("./templates/*.page.tmpl")
+	if err != nil {
+		return myCache, err
+	}
 
-//
+	//rage theough all fils ending wilt
+	for _, page := range pages {
+		name := filepath.Base(page)
+		ts, err := template.New(name).ParseFiles(page)
+
+		matches, err := filepath.Glob("./templates/*.layout.tmpl")
+		if err != nil {
+			return myCache, err
+		}
+
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob("./templates/*.layout.tmpl")
+			if err != nil {
+				return myCache, err
+			}
+		}
+		myCache[name] = ts
+	}
+	return myCache, nil
+
+}
+
+//var tc = make(map[string]*template.Template)
 //func RenderTemplate(w http.ResponseWriter, t string) {
 //	var tmpl *template.Template
 //	var err error
