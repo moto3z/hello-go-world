38c881e33af0bfa7102235e8e4423ebef99a2ef3
dong <dong.22@kakaoenterprise.com>
2023-09-04 09:49:30 +0900

    gg

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -1,13 +1,11 @@
 package render
 
 import (
-	"fmt"
 	"html/template"
-	"log"
 	"net/http"
 )
 
-func RenderTemplateV0(w http.ResponseWriter, tmpl string) {
+func RenderTemplate(w http.ResponseWriter, tmpl string) {
 	parsedTemplate, _ := template.ParseFiles("./templates/"+tmpl, "./templates/base.layout.tmpl")
 	err := parsedTemplate.Execute(w, nil)
 	if err != nil {
@@ -17,36 +15,37 @@ func RenderTemplateV0(w http.ResponseWriter, tmpl string) {
 
 var tc = make(map[string]*template.Template)
 
-func RenderTemplate(w http.ResponseWriter, t string) {
-	var tmpl *template.Template
-	var err error
-	//check see if we already have the template in our cache
-	_, inMap := tc[t]
-	if !inMap {
-		log.Println("creating template and adding to cache")
-		//need create this template
-		err = createTemplateCache(t)
-		if err != nil {
-			log.Println(err)
-		}
-	} else {
-		//have template in cache already
-		log.Println("using cache template")
-	}
-	tmpl = tc[t]
-	err = tmpl.Execute(w, nil)
-}
+//
+//func RenderTemplate(w http.ResponseWriter, t string) {
+//	var tmpl *template.Template
+//	var err error
+//	//check see if we already have the template in our cache
+//	_, inMap := tc[t]
+//	if !inMap {
+//		log.Println("creating template and adding to cache")
+//		//need create this template
+//		err = createTemplateCache(t)
+//		if err != nil {
+//			log.Println(err)
+//		}
+//	} else {
+//		//have template in cache already
+//		log.Println("using cache template")
+//	}
+//	tmpl = tc[t]
+//	err = tmpl.Execute(w, nil)
+//}
 
-func createTemplateCache(t string) error {
-	templates := []string{
-		fmt.Sprintln("./templates/%s", t),
-		"./templates/base.layout.tmpl",
-	}
-	tmpl, err := template.ParseFiles(templates...)
-	if err != nil {
-		return err
-	}
-
-	tc[t] = tmpl
-	return nil
-}
+//func createTemplateCache(t string) error {
+//	templates := []string{
+//		fmt.Sprintln("./templates/%s", t),
+//		"./templates/base.layout.tmpl",
+//	}
+//	tmpl, err := template.ParseFiles(templates...)
+//	if err != nil {
+//		return err
+//	}
+//
+//	tc[t] = tmpl
+//	return nil
+//}
