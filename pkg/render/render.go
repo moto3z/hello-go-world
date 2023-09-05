f367f98c9583cb6465dea3aaac801303d7a49d4c
dong <dong.22@kakaoenterprise.com>
2023-09-05 23:49:43 +0900

    ggff

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -29,37 +29,25 @@ func AddDefaultData(td *models.TemplateData) *models.TemplateData {
 func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData) {
 
 	var tc map[string]*template.Template
-	var err error
 
 	if app.UseCache {
+		// get the template cache from the app config
 		tc = app.TemplateCache
 	} else {
 		tc, _ = CreateTemplateCache()
 	}
 
 	//Step1 : create a template cache
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	//Step2 : get request from cache
 	t, ok := tc[tmpl]
 	if !ok {
-		fmt.Println(err)
 		log.Fatal("Could not get template from template cache")
 	}
 
 	buf := new(bytes.Buffer)
 	td = AddDefaultData(td)
-	err = t.Execute(buf, nil)
 	_ = t.Execute(buf, td)
 
-	if err != nil {
-		log.Println(err)
-	}
-
-	//Step3 : render the tmpl
-	_, err = buf.WriteTo(w)
+	_, err := buf.WriteTo(w)
 	if err != nil {
 		fmt.Println("error writing template to browser", err)
 	}
@@ -106,38 +94,3 @@ func CreateTemplateCache() (map[string]*template.Template, error) {
 	return myCache, nil
 
 }
-
-//var tc = make(map[string]*template.Template)
-//func RenderTemplate(w http.ResponseWriter, t string) {
-//	var tmpl *template.Template
-//	var err error
-//	//check see if we already have the template in our cache
-//	_, inMap := tc[t]
-//	if !inMap {
-//		log.Println("creating template and adding to cache")
-//		//need create this template
-//		err = createTemplateCache(t)
-//		if err != nil {
-//			log.Println(err)
-//		}
-//	} else {
-//		//have template in cache already
-//		log.Println("using cache template")
-//	}
-//	tmpl = tc[t]
-//	err = tmpl.Execute(w, nil)
-//}
-
-//func createTemplateCache(t string) error {
-//	templates := []string{
-//		fmt.Sprintln("./templates/%s", t),
-//		"./templates/base.layout.tmpl",
-//	}
-//	tmpl, err := template.ParseFiles(templates...)
-//	if err != nil {
-//		return err
-//	}
-//
-//	tc[t] = tmpl
-//	return nil
-//}
